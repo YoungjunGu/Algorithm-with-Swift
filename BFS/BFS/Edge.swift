5d5b6f2774cfaff38cbeebea85fe0eeb4ec9b207
gaki2745 <guyeongjun@naver.com>
2019-01-04 23:15:17 +0900

    Sort:Iterative Merge Sort with Swift

---
+++ BFS/BFS/Edge.swift
@@ -0,0 +1,14 @@
+//
+//  Edge.swift
+//  BFS
+//
+//  Created by youngjun goo on 02/01/2019.
+//  Copyright © 2019 youngjun goo. All rights reserved.
+//
+
+import Foundation
+
+
+public class Edge: CustomStringConvertible, Equatable {
+    
+}
